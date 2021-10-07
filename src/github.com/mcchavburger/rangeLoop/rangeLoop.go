6a9768baeb6d606b0fbbff026a2c9647097abc68
Dan Johnson <info@dojohnson.co.uk>
2021-10-07 21:48:08 +0100

    changed rangeLoop

--- src/github.com/mcchavburger/rangeLoop/rangeLoop.go
+++ src/github.com/mcchavburger/rangeLoop/rangeLoop.go
@@ -1,21 +1,25 @@
+//range looop
+//prints command line arguements
+//example "go run .\rangeLoop.go "test" "Something Else" " would print the inputs
+//same cexecution as basicLoop but shorter hand
+
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
+	s, sep := "", ""
+	//set the variabls s and sep to empty strings, dont need to define variable as string as "" is the default String value
+	//same as var s string = ""
+	for _, arg := range os.Args[1:] {
+		//range produces a pair of values, the index and the value of the element at that index
+		//above we assigning the index to _ and the value at that index to arg (_, arg := range os.Args[1:])
+		//_ is the "blank identifier", go does not permit unused local variables, however in this code as we do not need the index, we assign to blank and avoid an error
+		s += sep + arg
+		sep = " "
 	}
-	//ignoring potential errors from input.Err()
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s \n", n, line)
-		}
-	}
+	fmt.Println(s)
 }
